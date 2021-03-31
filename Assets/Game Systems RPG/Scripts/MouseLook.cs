43c832ae6978602eeafa14316bdf4a0f2c73a90f
jtbarker92 <78832084+jtbarker92@users.noreply.github.com>
2021-04-01 10:41:29 +1100

    Initial 2

---
+++ Assets/Game Systems RPG/Scripts/MouseLook.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Errors : 17
+namespace Debugging.Player
+{
+    [AddComponentMenu("RPG/Player/Mouse Look")]
+    public class MouseLook : MonoBehaviour
+    {
+        public enum RotationalAxis
+        {
+            MouseX,
+            MouseY
+        }
+        
+        [Header("Rotation Variables")]
+        public RotationalAxis axis = RotationalAxis.MouseX;
+        [Range(0,200)]
+        public float sensitivity = 100;
+        public float minY = -60, maxY = 60;
+        private float _rotY;
+
+
+
+        void Start()
+        {
+            if(GetComponent<Rigidbody>())
+            {
+                GetComponent<Rigidbody>().freezeRotation = true;
+            }
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            if(GetComponent<Camera>())
+            {
+                axis = RotationalAxis.MouseY;
+            }
+        }
+        void Update()
+        {
+            if(axis == RotationalAxis.MouseX)
+            {
+                transform.Rotate(0,Input.GetAxisRaw("Mouse X") * sensitivity * Time.deltaTime,0);
+            }
+            else
+            {
+                _rotY += Input.GetAxisRaw("Mouse Y")  * sensitivity * Time.deltaTime;
+                _rotY = Mathf.Clamp(_rotY,minY,maxY);
+                transform.localEulerAngles = new Vector3(-_rotY,0,0);
+            }
+        }
+    }   
+}
